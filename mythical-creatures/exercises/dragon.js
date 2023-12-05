42c664d248170700e614b97a036e8cb4dc298c07
Edwin Chalmers <edwinchalmers@gmail.com>
2023-12-04 20:51:53 -0700

    Answer createDragon greetRider, working on eat

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,24 @@
+function createDragon(name, rider, temperment) {
+  return {
+    name,
+    rider,
+    temperment
+  } 
+}
 
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`
+}
+
+function eat(dragon) {
+  dragon.timesEaten += 1;
+  dragon.hungry = true;
+  return dragon
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
+  createDragon, 
+  greetRider, 
+  eat, 
   // findFireBreathers
 }
